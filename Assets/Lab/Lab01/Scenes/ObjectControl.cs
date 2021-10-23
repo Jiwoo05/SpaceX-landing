ad2b8febf3d19eba388ce3b5566c4db8e421c3d5
taeryu0627 <taeryu0627@naver.com>
2021-10-23 17:59:38 +0900

    Lab01jiwoo

---
+++ Assets/Lab/Lab01/Scenes/ObjectControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectControl : MonoBehaviour
+{
+    public int a = 1;
+    public int b = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(b.ToString());
+        if (Input.GetKey(KeyCode.Space))
+        { 
+            a = a + b;
+            transform.position = new Vector3(a, 0, 0);
+        }
+        else
+        {
+            transform.position = new Vector3(a, 0, 0);
+        }
+    }
+}
